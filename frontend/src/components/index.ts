c70bbb0a4308d9eaa972e140cd187a168d803bd2
Hoang Bui <buinguyenhoang0103@gmail.com>
2025-06-01 09:52:35 +0700

    remove booking example

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,4 +3,3 @@ export { WalletButton } from "./WalletButton";
 export { WalletConnectionModal } from "./WalletConnectionModal";
 export { TicketList } from "./TicketList";
 export { TicketAccordion } from "./TicketAccordion";
-export { BookingExample } from "./BookingExample";
